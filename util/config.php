4f413575dec42c2de11b7ff63ac62a54e468f9c2
Heloisa T. L. <heloisa.tonin.tds2023@gmail.com>
2025-10-28 16:52:03 -0300

    configuração para funcionar no meu pc

--- util/config.php
+++ util/config.php
@@ -5,10 +5,10 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 //Configurar essas variáveis de acordo com o seu ambiente
-define("DB_HOST", "mysql-server");
-define("DB_NAME", "db_alunos");
+define("DB_HOST", "localhost");
+define("DB_NAME", "db_alunos2");
 define("DB_USER", "root");
-define("DB_PASSWORD", "root");
+define("DB_PASSWORD", "");
 
 //Configuração do ambiente
 define("AMB_DEV", true);
